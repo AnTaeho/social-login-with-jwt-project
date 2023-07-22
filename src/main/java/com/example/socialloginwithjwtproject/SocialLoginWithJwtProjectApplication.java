ab541218a8f283b6ebdf96322205947b955d48dd
antaeho <an981022@naver.com>
2023-07-22 14:16:15 +0900

    Initial commit

---
+++ src/main/java/com/example/socialloginwithjwtproject/SocialLoginWithJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialloginwithjwtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginWithJwtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginWithJwtProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialloginwithjwtproject/SocialLoginWithJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialloginwithjwtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWithJwtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
